373aa3c1c4b20b5f23a0105729c9d296b4136a89
Tim Jalgasbaev <t.jalgasbaev@stylemix.net>
2021-11-06 14:13:06 +0500

    API, валюта и компонент

--- src/main.js
+++ src/main.js
@@ -4,28 +4,31 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import dateFilter from '@/filters/date.filter'
+import currencyFilter from '@/filters/currency.filter'
 import messagePlugin from '@/utils/message.plugin'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
+import Loader from '@/components/app/Loader'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
-// console.log(initializeApp.auth) // Undefined
-// console.log(initializeApp.default.auth) // Function
 
 Vue.config.productionTip = false
 Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
+Vue.filter('currency', currencyFilter)
+Vue.component('Loader', Loader)
 
 firebase.initializeApp({
   apiKey: "AIzaSyBxV4Tw1Uuei8k0W-W007uAwGC0nxA0uTo",
   authDomain: "vue-crm-7915b.firebaseapp.com",
+  databaseURL: "https://vue-crm-7915b-default-rtdb.firebaseio.com",
   projectId: "vue-crm-7915b",
   storageBucket: "vue-crm-7915b.appspot.com",
   messagingSenderId: "393490489297",
-  appId: "1:393490489297:web:d6a64d3e25166931fecaf0",
-  measurementId: "G-DY2NTSJ2D0"
+  appId: "1:393490489297:web:a28c93639ed3d0b4fecaf0",
+  measurementId: "G-2BVB9DE8CM"
 })
 
 let app
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -99,7 +99,7 @@ export default {
     agree: {checked: v => v}
   },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch()
         return
@@ -108,11 +108,13 @@ export default {
       const formData = {
         email: this.email,
         password: this.password,
-        name: this.email
+        name: this.name
       }
 
-      console.log(formData);
-      this.$router.push('/');
+      try {
+        await this.$store.dispatch('register', formData)
+        this.$router.push('/');
+      } catch (e) {}
     }
   }
 }
